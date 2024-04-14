778bd53c79982a2e97193bb0f5f00f02fb8d2f65
GayathriCodes2 <gayathricodes2@gmail.com>
2024-04-15 01:28:06 +0530

    find no of 5's in 2d Arr

--- try.js
+++ try.js
@@ -1,7 +1,10 @@
-arr = 5
-count =0;
-for(j=0;j<=31;j++){
-    if ((arr & (1 << j)) !== 0) {
-        count++
+arr = [[1,2,5],[3,5,5]]
+out = [];
+count = 0
+for(i=0;i<arr.length;i++){
+    for(j=0;j<arr[i].length;j++){
+        if(arr[i][j]===5){
+            count++
+        }
     }
 }console.log(count)
