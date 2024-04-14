bc016b2d5bcc28943210c4225f2437cba1000d23
GayathriCodes2 <gayathricodes2@gmail.com>
2024-04-15 01:24:40 +0530

    find the no of bit for single digit

--- try.js
+++ try.js
@@ -1,24 +1,7 @@
-arr =[5,7,12,9, 15]
-n=arr.length
-out = []
-for(i=0;i<n;i++){
-    count =0;
+arr = 5
+count =0;
 for(j=0;j<=31;j++){
-    if ((arr[i] & (1 << j)) !== 0) {
+    if ((arr & (1 << j)) !== 0) {
         count++
     }
-}out.push(count);
-}
-for(let i=0;i<n-1;i++){
-    for(j=0;j<=n-i-1;j++){
-if(out[j]<out[j+1]){
-temp = out[j];
-out[j]=out[j+1]
-out[j+1]=temp;
-
-temp = arr[j];
-arr[j]=arr[j+1]
-arr[j+1]=temp;
-}
-}
-}console.log(arr,out)
+}console.log(count)
